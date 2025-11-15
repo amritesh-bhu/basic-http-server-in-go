1853b1cf497f0cf761c14d7946f033c76f9b732a
amritesh-bhu <amriteshbhu438@gmail.com>
2025-11-16 01:36:37 +0530

    implement error handling at route handler

--- src/route-handlers/post.go
+++ src/route-handlers/post.go
@@ -2,67 +2,108 @@ package routehandlers
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"strconv"
 
 	"example.com/go-mongo-crud/src/domain"
+	"example.com/go-mongo-crud/src/helper"
 )
 
 func HandleGetPosts(w http.ResponseWriter, r *http.Request) {
 	posts := domain.GetAllPosts()
 
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(posts)
+	json.NewEncoder(w).Encode(map[string]any{
+		"message": "all posts",
+		"posts":   posts,
+	})
 }
 
 func HandleGetPost(w http.ResponseWriter, r *http.Request) {
-	id, _ := strconv.Atoi(r.URL.Query().Get("id"))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 
-	post, ok := domain.GetPostByID(id)
-	if !ok {
-		http.Error(w, "Post not found", http.StatusNotFound)
+	if err != nil {
+		helper.WriteError(w, "missing id query parameter", http.StatusBadRequest)
+		return
+	}
+
+	post, err := domain.GetPostByID(id)
+	if err != nil {
+		helper.WriteError(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(post)
+	json.NewEncoder(w).Encode(map[string]any{
+		"messgage": fmt.Sprintf("post for id: %s", id),
+		"post":     post,
+	})
 }
 
 func HandleCreatePost(w http.ResponseWriter, r *http.Request) {
 	var p domain.Post
 	json.NewDecoder(r.Body).Decode(&p)
 
+	if p.Title == "" || p.Content == "" {
+		helper.WriteError(w, "content or title is empty", http.StatusBadRequest)
+		return
+	}
+
 	post := domain.CreatePost(p)
 
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(post)
+	json.NewEncoder(w).Encode(map[string]any{
+		"message": "post created",
+		"post":    post,
+	})
 }
 
 func HandleUpdatePost(w http.ResponseWriter, r *http.Request) {
-	id, _ := strconv.Atoi(r.URL.Query().Get("id"))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+
+	if err != nil {
+		helper.WriteError(w, "missing parameter id", http.StatusBadRequest)
+		return
+	}
 
 	var p domain.Post
-	json.NewDecoder(r.Body).Decode(&p)
+	errJson := json.NewDecoder(r.Body).Decode(&p)
 
-	post, ok := domain.UpdatePost(id, p)
-	if !ok {
-		http.Error(w, "Post not found", http.StatusNotFound)
+	if errJson != nil {
+		helper.WriteError(w, "invalid json body", http.StatusInternalServerError)
+		return
+	}
+
+	post, err := domain.UpdatePost(id, p)
+	if err != nil {
+		helper.WriteError(w, err.Error(), http.StatusNotFound)
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(post)
+	json.NewEncoder(w).Encode(map[string]any{
+		"message": "post updated",
+		"post":    post,
+	})
 }
 
 func HandleDeletePost(w http.ResponseWriter, r *http.Request) {
-	id, _ := strconv.Atoi(r.URL.Query().Get("id"))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 
-	ok := domain.DeletePost(id)
-	if !ok {
-		http.Error(w, "Post not found", http.StatusNotFound)
+	if err != nil {
+		helper.WriteError(w, "missing parameter id", http.StatusBadRequest)
+		return
+	}
+
+	post := domain.DeletePost(id)
+	if post != nil {
+		helper.WriteError(w, post.Error(), http.StatusNotFound)
 		return
 	}
 
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Post deleted"))
+	json.NewEncoder(w).Encode(map[string]string{
+		"message": "post deleted",
+	})
 }
